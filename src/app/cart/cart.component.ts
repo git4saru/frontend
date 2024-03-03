b85f0f0d7a010c754b79ab3994f51f014906bc89
Sarutha Vignesh <emailsaruthavignesh@gmail.com>
2024-03-03 14:42:00 +0530

    cart page completed

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../model/product';
+import { CartService } from '../service/cart.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  public get cartService(): CartService {
+    return this._cartService;
+  }
+  public set cartService(value: CartService) {
+    this._cartService = value;
+  }
+  cartItems: Product[] = [];
 
+  constructor(private _cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.cart$.subscribe((items: Product[]) => {
+      this.cartItems = items;
+    });
+  }
+
+  removeFromCart(product: Product) {
+    this.cartService.removeFromCart(product);
+  }
+
+  getTotal(): number {
+    return this.cartService.getTotal();
+  }
 }
