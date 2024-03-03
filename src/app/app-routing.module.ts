fe63c7230eaa212f7d73f074a5503045172dbea9
Sarutha Vignesh <emailsaruthavignesh@gmail.com>
2024-03-03 17:04:52 +0530

    checkout completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AdminDashboardComponent } from './component/admin-dashboard/admin-dashboard.component';
 import { UserDashboardComponent } from './component/user-dashboard/user-dashboard.component';
 import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'signup', component: SignupComponent},
   {path: 'user', component: UserDashboardComponent},
   {path: 'admin', component:AdminDashboardComponent},
-  {path: 'cart', component:CartComponent}
+  {path: 'cart', component:CartComponent},
+  {path: 'checkout', component: CheckoutComponent } 
 ];
 
 @NgModule({
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../model/product';
 import { CartService } from '../service/cart.service';
+import { Router } from '@angular/router'; // Import Router for navigation
 
 @Component({
   selector: 'app-cart',
@@ -19,7 +20,7 @@ export class CartComponent implements OnInit {
   }
   cartItems: Product[] = [];
 
-  constructor(private _cartService: CartService) { }
+  constructor(private _cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
     this.cartService.cart$.subscribe((items: Product[]) => {
@@ -34,4 +35,9 @@ export class CartComponent implements OnInit {
   getTotal(): number {
     return this.cartService.getTotal();
   }
+  checkout() {
+    // Navigate to the checkout page
+    this.router.navigate(['/checkout']);
+  }
+
 }
